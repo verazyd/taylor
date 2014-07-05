e583539f45a8ceb9b04fcd2152003c7040b8b70b
root <root@ip-172-31-38-126.us-west-2.compute.internal>
2014-07-05 04:35:26 +0000

    conflict resolved

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -15,8 +15,10 @@ class SearchController extends \BaseController {
 //
 //        }
         $query = Request::get('query');
+
         DB::statement('CREATE OR REPLACE VIEW victories_view_name  AS SELECT id, topic, description, created_at, updated_at FROM victories');
         DB::statement('CREATE OR REPLACE VIEW blogs_view  AS SELECT id, topic, description, created_at, updated_at FROM blogs');
+
         if($query)
         {
             $victories = DB::table('victories_view_name')->where('topic', 'LIKE', "%$query%");
