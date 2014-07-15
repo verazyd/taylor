541fbe93ec1e807c79e7e49c0b5b2e593978a639
shankargiri <shankargiri.p@gmail.com>
2014-07-15 23:05:38 +1000

    videos embeded for video center from youtube

--- app/views/videos/index_users.blade.php
+++ app/views/videos/index_users.blade.php
@@ -1,7 +1,47 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container" style="margin: 1%">
-        Video Page Under Construction.....
+<div style="margin: 1px;">
+    <h2>Video Center</h2>
+       <div class="row">
+           <div class="col-md-6">
+               <iframe width="630" height="315" src="//www.youtube.com/embed/TdM2bcK7yqY?list=UUeRHEcixuL-zQ2ygvFILWDQ" frameborder="0" allowfullscreen></iframe>
+           </div>
+           <div class="col-md-6">
+               <iframe width="630" height="315" src="//www.youtube.com/embed/3U3K4CVldkk?list=UUeRHEcixuL-zQ2ygvFILWDQ" frameborder="0" allowfullscreen></iframe>
+           </div>
+       </div>
+    <div class="row">
+        <div class="col-md-6">
+            <iframe width="630" height="315" src="//www.youtube.com/embed/zj7ImIhgs8g?list=UUeRHEcixuL-zQ2ygvFILWDQ" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-6">
+            <iframe width="630" height="315" src="//www.youtube.com/embed/KiGMnP4AEE4?list=UUeRHEcixuL-zQ2ygvFILWDQ" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <iframe width="630" height="315" src="//www.youtube.com/embed/MRF3wWOnnM8?list=UUeRHEcixuL-zQ2ygvFILWDQ" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-6">
+            <iframe width="630" height="315" src="//www.youtube.com/embed/FrEEGxiaF-w?list=UUeRHEcixuL-zQ2ygvFILWDQ" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <iframe width="630" height="315" src="//www.youtube.com/embed/31ecVfCoIK4?list=UUeRHEcixuL-zQ2ygvFILWDQ" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-6">
+            <iframe width="630" height="315" src="//www.youtube.com/embed/tmllhGQe_bM?list=UUeRHEcixuL-zQ2ygvFILWDQ" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <iframe width="630" height="315" src="//www.youtube.com/embed/cF3DHhPtbXI?list=UUeRHEcixuL-zQ2ygvFILWDQ" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-6">
+            <iframe width="630" height="315" src="//www.youtube.com/embed/tikezms37Gc?list=UUeRHEcixuL-zQ2ygvFILWDQ" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
 </div>
 @stop
